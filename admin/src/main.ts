b2509e09561981368133a12ba062b3b531c6d445
tashckinov <33600944+tashckinov@users.noreply.github.com>
2026-07-28 23:53:30 +0300

    Enable automatic anchor segment rebuilding

--- admin/src/main.ts
+++ admin/src/main.ts
@@ -5,7 +5,9 @@ import './route-sequence-enhancer.css'
 import { createApp } from 'vue'
 
 import App from './App.vue'
+import { initRoadAnchorRebuild } from './road-anchor-rebuild'
 import { initRouteSequenceEnhancer } from './route-sequence-enhancer'
 
 createApp(App).mount('#app')
 initRouteSequenceEnhancer()
+initRoadAnchorRebuild()
